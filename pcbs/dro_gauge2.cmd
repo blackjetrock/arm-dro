e403dfb36a93fb31103069b8a2513990eb55cd45
blackrockjet <menadue@gmail.com>
2019-04-17 15:18:20 +0100

    Added latest gauge PCB and schematic

---
+++ pcbs/dro_gauge2.cmd
@@ -0,0 +1,101 @@
+# Pin name action command file
+
+# Start of element C4
+ChangePinName(C4, 2, 2)
+ChangePinName(C4, 1, 1)
+
+# Start of element CONN2
+ChangePinName(CONN2, 5, 5)
+ChangePinName(CONN2, 3, 3)
+ChangePinName(CONN2, 1, 1)
+ChangePinName(CONN2, 6, 6)
+ChangePinName(CONN2, 4, 4)
+ChangePinName(CONN2, 2, 2)
+
+# Start of element Q4
+ChangePinName(Q4, 2, B)
+ChangePinName(Q4, 1, E)
+ChangePinName(Q4, 3, C)
+
+# Start of element Q3
+ChangePinName(Q3, 2, B)
+ChangePinName(Q3, 1, E)
+ChangePinName(Q3, 3, C)
+
+# Start of element C3
+ChangePinName(C3, 2, 2)
+ChangePinName(C3, 1, 1)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element R10
+ChangePinName(R10, 1, 1)
+ChangePinName(R10, 2, 2)
+
+# Start of element R9
+ChangePinName(R9, 1, 1)
+ChangePinName(R9, 2, 2)
+
+# Start of element R8
+ChangePinName(R8, 1, 1)
+ChangePinName(R8, 2, 2)
+
+# Start of element R7
+ChangePinName(R7, 1, 1)
+ChangePinName(R7, 2, 2)
+
+# Start of element Q2
+ChangePinName(Q2, 2, 2)
+ChangePinName(Q2, 1, 1)
+ChangePinName(Q2, 3, 3)
+
+# Start of element Q1
+ChangePinName(Q1, 2, 2)
+ChangePinName(Q1, 1, 1)
+ChangePinName(Q1, 3, 3)
+
+# Start of element R6
+ChangePinName(R6, 1, 1)
+ChangePinName(R6, 2, 2)
+
+# Start of element R5
+ChangePinName(R5, 1, 1)
+ChangePinName(R5, 2, 2)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element CONN1
+ChangePinName(CONN1, 4, 4)
+ChangePinName(CONN1, 3, 3)
+ChangePinName(CONN1, 2, 2)
+ChangePinName(CONN1, 1, 1)
+
+# Start of element U1
+ChangePinName(U1, 5, RA2)
+ChangePinName(U1, 6, RA1/ICSPCLK)
+ChangePinName(U1, 7, RA0/ICSPDAT)
+ChangePinName(U1, 8, VSS)
+ChangePinName(U1, 4, MCLR/VPP/RA3)
+ChangePinName(U1, 3, RA4)
+ChangePinName(U1, 2, RA5)
+ChangePinName(U1, 1, VDD)
